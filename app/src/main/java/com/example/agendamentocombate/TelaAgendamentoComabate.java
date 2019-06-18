c08860eae9a418dedde4eb5678f2f8eaee11670b
Felipe Baumgartel <felipe.baumgartel@catolicasc.org.br>
2019-06-18 20:02:52 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/agendamentocombate/TelaAgendamentoComabate.java
+++ app/src/main/java/com/example/agendamentocombate/TelaAgendamentoComabate.java
@@ -1,10 +1,13 @@
 package com.example.agendamentocombate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TelaAgendamentoComabate extends AppCompatActivity {
 
@@ -12,7 +15,17 @@ public class TelaAgendamentoComabate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_agendamento_comabate);
+
+        Intent intent = getIntent();
+
+        btSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
+
     TextView tvRobo1 = findViewById(R.id.tvRobo1);
     TextView tvRobo2 = findViewById(R.id.tvRobo2);
     EditText etData = findViewById(R.id.etData);
